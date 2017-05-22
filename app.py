5f84fa65b217b70395c52b7e6dc7181564c47673
Adrian Kim <adrikim@umich.edu>
2017-05-21 21:39:10 -0400

    Initial html/css/flask routing for English search

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/search/en')
+def render():
+    return render_template('search_en.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
