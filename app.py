bcc306f99a0ea648e061157a03ba0bf61f59945d
Adrian Kim <adrikim@umich.edu>
2017-05-22 18:40:42 -0400

    Added initial search page static header, static menu drawer

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
-@app.route('/search/en')
+
+@app.route('/search')
 def render():
     return render_template('search_en.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
