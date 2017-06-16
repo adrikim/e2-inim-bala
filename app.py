0106f42aaa07fd6f71595f7497e1a2c3b9fee731
Adrian Kim <adrikim@umich.edu>
2017-06-15 21:01:43 -0400

    add starter for scraper, templates

--- app.py
+++ app.py
@@ -1,15 +1,85 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    """
+    Renders the search homepage.
+    """
+    return render_template("index.html")
 
 
-@app.route('/search')
-def render():
-    return render_template('search_en.html')
+@app.route('/search', methods=['GET'])
+def search(query=None, searchType=None):
+    """
+    Returns search results based on query method chosen.
+
+    Parameters:
+    {
+        query: string,
+        searchType: { EN | SUM | AKK | SIGN },
+        eng: {
+            include: string,
+            exclude: string
+        },
+        sum: {
+            containsSign: [ string, ... ],
+            containsDet: [ string, ... ],
+            startsWith: string,
+            endsWith: string,
+            enableHomophones: bool
+        }
+    }
+    """
+    # parse/clean up query
+    # construct db query
+    # search with query
+    # return results in webpage
+    return render_template("search.html")
+
+
+@app.route('/signsearch')
+def signsearch(query=None):
+    """
+    Returns sign search results on righthand pane of main site.
+
+    Parameters:
+    {
+        query: string,
+        startsWith: string,
+        endsWith: string,
+        contained: string,
+        times: string,
+        gunu: bool,
+        sessig: bool,
+        enableHomophones: bool
+    }
+    """
+    # parse/clean up query
+    # construct db query
+    # search with query
+    # return JSON result
+    pass
+
+
+@app.route('/sign/<id>')
+def get_sign(id=None):
+    if id is None:
+        return "Error: must specify id"
+    return render_template("sign.html")
+
+
+@app.route('/entry/<id>')
+def get_entry(id=None):
+    if id is None:
+        return "Error: must specify id"
+    return render_template("entry.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
---
+++ grab_data.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+
+EPSD_ENTRY_URL = 'http://psd.museum.upenn.edu/epsd/epsd/e{}.html'
+ENTRY_NUMBERS = [ x for x in range(1, 6610) ]
+# ENTRY_LETTERS = [ 'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'k', 'l', 'm', 'n',
+#                  'ng', 'p', 'r', 's', 'sh', 't', 'u', 'w', 'y', 'z']
+
+
+def get_entries():
+    """
+    Grabs all entries from each e<LETTER>s.html page and
+    returns them as a list of JSONs.
+
+    Page's format is:
+    h1 .entry
+        span .cf
+        span .gw
+    p .icount
+
+    div .summary
+        p
+            a (unimportant)
+            "time periods"
+            span .wr (...)
+                sub (for subscript #'s')
+            (...)
+            "description"
+            span .akk (...)
+        div .compounds
+            p .xcpd
+                "See"
+                a href="javascript:showarticle('e<ID#>.html')"
+                    "canon_form[guide_word]",
+                (...)
+    div .orth
+    div .orth oview
+    div .morphology
+    div .senses
+    div .glosses
+    div .bib
+    p .outlink
+    """
+    for x in ENTRY_NUMBERS:
+        entry = { 'id': x }
+        url = EPSD_ENTRY_URL.format(x)
+        print("Getting entries from ", url, "...")
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser") # is the page's DOM
+
+        # get entry's short form
+        entry['canonForm'] = soup.find("h1", { "class": "entry" }).find("span", { "class": "cf" }).text.strip() # no numbers
+        entry['guideWord'] = soup.find("h1", { "class": "entry" }).find("span", { "class": "gw" }).text.strip()
+
+        # get instance count
+        entry['instances'] = soup.find("p", { "class": "icount" }).text.strip().rstrip(" instances)").lstrip("(")
+
+        # get summary
+        # (399x: ED IIIb, Old Akkadian, Lagash II, Ur III, Old Babylonian, unknown)  wr. e; na-be2-a; be2; ne; da-me; na-be2; e7 "perfect plural and imperfect stem of dug[to speak]"
+        summary = soup.find("div", { "class": "summary" }).find("p").text
+        # NOTE all the info here is found in other sections anyway...
+        # TODO get time periods
+        # TODO get ortho forms here?
+
+        # get compounds
+        compounds = soup.find("div", { "class": "summary" }).find("div", { "class": "compounds" }).find("p")
+        entry['compounds'] = [ { a: a.text } for a in compounds.find_all("a") ] # { id#: citationForm[guideWord] }
+        '''
+             (<INSTANCES>x: ED IIIa, ED IIIb, Old Akkadian, Lagash II, Ur III, Early Old Babylonian, Old Babylonian)  wr.
+            <span class="wr">
+                a<sub>2</sub>
+            </span>
+             "arm; labor; wing; horn; side; strength; wage; power" Akk.&nbsp;
+        '''
+        # TODO get orthography
+        # TODO get orthography overview
+        # TODO get morphology/attested forms
+        # TODO get senses/meanings
+        # get Akkadian glosses
+        akk = soup.find("div", { "class": "glosses" }).text.lstrip(".\n").rstrip("\nAkk.\xa0")
+        entry['akkadian'] = []
+        for k in akk.split("\"; "):
+            s = k.split(" \"", 1)
+            entry['akkadian'].append({ 'word': s[0], 'meaning': s[1]})
+        # TODO get bibliography
+        bib = soup.find("div", { "class": "bib" })
+        # TODO get outlinks
+        outlinks = soup.find("p", { "class": "outlink" })
+        if outlinks.text:
+
+
+if (__name__ == "__main__"):
+    get_entries()
+    pass
